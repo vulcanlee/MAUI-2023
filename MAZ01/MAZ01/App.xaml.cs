e58819eee93d764356a825963d1438ac47a15fe4
Vulcan <vulcan.lee@gmail.com>
2023-02-06 11:10:48 +0800

    MAZ01|CRUD 的完整開發範例建立

---
+++ MAZ01/MAZ01/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAZ01;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MAZ01/MAZ01/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAZ01.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
